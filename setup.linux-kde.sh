02e2f63c4a8094ea30af9d473f0b5f030e2ab7da
r0mankon <r0mankon.me@gmail.com>
2023-09-10 11:28:03 +0600

    linux update

--- setup.linux-kde.sh
+++ setup.linux-kde.sh
@@ -5,8 +5,10 @@ cp ./r0mankon.profile ~/.local/share/konsole/r0mankon.profile
 cat ./xinitrc >> ~/.xinitrc
 apt install gnome-keyring libsecret libgnome-keyring
 
-apt install yakuake kio-gdrive libdbusmenu-glib4 kdenetwork-filesharing  xdg-desktop-portal xdg-desktop-portal-kde -y
+apt install software-properties-qt yakuake kio-gdrive libdbusmenu-glib4 kdenetwork-filesharing  xdg-desktop-portal xdg-desktop-portal-kde -y
 
 # file picker for snap/flatpak apps
 mkdir -p "$HOME/.config/plasma-workspace/env"
 echo "export GTK_USE_PORTAL=1" >> "$HOME/.config/plasma-workspace/env/gtk_use_portal.sh"
+
+./setup.linux.sh
